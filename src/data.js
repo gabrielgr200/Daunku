6a7ff5ad4efa2bebd541fb090be88420c134f233
Leirbag <gomesrdg34@gmail.com>
2023-12-20 17:01:44 -0300

    planta

---
+++ src/data.js
@@ -0,0 +1,113 @@
+import shippingIcon from "./assets/free-shiping-icon.svg";
+import paymentIcon from "./assets/quick-payment-icon.svg";
+import supportIcon from "./assets/support-icon.svg";
+import productOneIcon from "./assets/product-one.svg";
+import productTwoIcon from "./assets/produc-two.svg";
+import productThreeIcon from "./assets/product-three.svg";
+import plantIcon from "./assets/plant-icon.svg";
+import sunIcon from "./assets/sun-icon.svg";
+import waterIcon from "./assets/water-icon.svg";
+import temperatureIcon from "./assets/temperature-icon.svg";
+
+export const heroTitle = "Bring Serenity to Your Place With Interior Plants";
+export const heroSubtitle =
+  "find your dream plant for your home decoration with us, and we will make it happen.";
+
+export const services = [
+  {
+    title: "Frete grátis",
+    subtitle: "Não há custo para sua entrega",
+    icon: shippingIcon,
+  },
+  {
+    title: "Pagamento rápido",
+    subtitle: "100% seguro",
+    icon: paymentIcon,
+  },
+  {
+    title: "Suporte 24/7",
+    subtitle: "Não hesite em contactar-nos",
+    icon: supportIcon,
+  },
+];
+
+export const productsTitle = "Produtos mais vendidos";
+export const productsBtnText = "Ver coleção";
+
+export const products = [
+  {
+    title: "Pothos Dourado",
+    price: "R$65.99",
+    img: productOneIcon,
+  },
+  {
+    title: "Planta Rubber",
+    price: "R$75.00",
+    img: productTwoIcon,
+  },
+  {
+    title: "Planta Cast Iron",
+    price: "R$88.50",
+    img: productThreeIcon,
+  },
+];
+
+export const referenceTitle = "Referência de plantas de interior";
+export const referenceSubtitle = "Deixe sua casa aconchegante com plantas refrescantes";
+
+export const careTitle = "Como cuidar das plantas";
+export const careSubtitle = "Cuide das plantas de todo o coração";
+
+export const careList = [
+  {
+    title: "Ajustar a iluminação",
+    subtitle:
+      "Ao cuidar de plantas domésticas, certifique-se de que a temperatura ambiente não esteja nem muito fria nem muito quente.",
+    img: sunIcon,
+  },
+  {
+    title: "Não regue com muita frequência",
+    subtitle:
+      "A rega de plantas ornamentais não precisa ser feita todos os dias.",
+    img: waterIcon,
+  },
+  {
+    title: "Escolha o recipiente certo.",
+    subtitle:
+      "As plantas de casa precisarão de recipientes diferentes dependendo do tipo e tamanho.",
+    img: plantIcon,
+  },
+  {
+    title: "Fertilize regularmente",
+    subtitle:
+      "The nutrients most indoor houseplants need are nitrogen for balance and potassium for stem strength.",
+    img: temperatureIcon,
+  },
+];
+
+export const newsletter = "Newsletter";
+
+export const footerLinksColumnOne = [
+  {
+    title: "Suporte",
+    links: ["Sobre nós", "Carreiras", "Blog"],
+  },
+  {
+    title: "Links Úteis",
+    links: ["Pagamento e impostos", "Termos de serviço", "política de Privacidade"],
+  },
+];
+
+export const footerLinksColumnTwo = [
+  {
+    title: "Nosso cardápio",
+    links: ["Melhor produto", "Categorias"],
+  },
+  {
+    title: "Endereço",
+    links: [
+      "Divinópolis, 123 Maria Helena, Brasil",
+      "hallo@daunku.com",
+    ],
+  },
+];
